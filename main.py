5f3d53ddd19ef90e26d3b456887d9ac4a44f5ea9
Jesse Li <jesse.li2002@gmail.com>
2019-10-02 22:40:42 -0700

    Now ready for proper tinkering

--- main.py
+++ main.py
@@ -37,18 +37,12 @@ with open(os.path.join(log_fold, "datalog_" + now.strftime("%Y%m%d_%H%M%S") + ".
     log_writer.writerow(("Time", "Visible", "Uncovered", "Warnings"))
     times_uncovered = 0
 
-    times_total = 6 * 24 * 30  # once every 10 minutes for 24 hrs for 30 days
-
-    last_day = None
+    times_total = 6 * 6  # once every 10 minutes for 24 hrs for 30 days
 
     # Skyfield recommends using a single time object, but all the other math is so much easier if it's just one time
+
     times = (ts.utc(2019, 9, 23, 0, 10 * x) for x in range(times_total))
     for t in times:
-
-        # if t.day != last_day:
-        #     last_day = t.day
-        #     print(f"Now simulating {t}")
-
         num_vis, num_uncovered = rocket.numVisibleUncovered(
             gps_sats, t, l, True)
 
@@ -62,12 +56,6 @@ with open(os.path.join(log_fold, "datalog_" + now.strftime("%Y%m%d_%H%M%S") + ".
 
         log_writer.writerow((str(t), num_vis, num_uncovered, warning))
 
-
-
-
-
-
-    log_writer.writerow((str(t), num_vis, num_uncovered, warning))
 
 print(f"{times_uncovered / times_total * 100:.2}% downtime.")
 input("\nPress enter to finish.")
