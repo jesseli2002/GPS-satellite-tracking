eef08c402087e3b3cc3184feb81da1aec7b7113f
Jesse Li <jesse.li2002@gmail.com>
2019-10-02 22:36:00 -0700

    something

--- main.py
+++ main.py
@@ -28,15 +28,18 @@ with open(os.path.join(log_fold, "datalog_" + now.strftime("%Y%m%d_%H%M%S") + ".
     log_writer = csv.writer(log_file)
     log_writer.writerow(("Time", "Visible", "Uncovered", "Warnings"))
     times_uncovered = 0
+
     times_total = 6 * 24 * 30  # once every 10 minutes for 24 hrs for 30 days
-    time_start = dt.datetime(2019, 9, 23, 4)
+
     last_day = None
 
-    for i in range(times_total):
-        t = time_start + dt.timedelta(0, i * 600)
-        if t.day != last_day:
-            last_day = t.day
-            print(f"Now simulating {t}")
+    # Skyfield recommends using a single time object, but all the other math is so much easier if it's just one time
+    times = (ts.utc(2019, 9, 23, 0, 10 * x) for x in range(times_total))
+    for t in times:
+
+        # if t.day != last_day:
+        #     last_day = t.day
+        #     print(f"Now simulating {t}")
 
         num_vis, num_uncovered = rocket.numVisibleUncovered(
             gps_sats, t, l, True)
@@ -50,6 +53,13 @@ with open(os.path.join(log_fold, "datalog_" + now.strftime("%Y%m%d_%H%M%S") + ".
             times_uncovered += 1
 
         log_writer.writerow((str(t), num_vis, num_uncovered, warning))
+
+
+
+
+
+
+    log_writer.writerow((str(t), num_vis, num_uncovered, warning))
 
 print(f"{times_uncovered / times_total * 100:.2}% downtime.")
 input("\nPress enter to finish.")
